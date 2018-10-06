da0e4bce58ec0945ca1d434ed5f66f3f5e01214b
Fudgyking <escalado19@hotmail.com>
2018-10-06 11:26:45 -0400

    Fix mining and house

--- bot/bot.py
+++ bot/bot.py
@@ -37,7 +37,9 @@ class Bot:
         try:
             for i in range(len(self.moves)):
                 tile = gameMap.getTileAt(self.PlayerInfo.Position + self.moves[i])
-                if tile == TileContent.Resource:
+                if tile == TileContent.House and self.PlayerInfo.CarriedResources > 0:
+                    return create_move_action(self.moves[i])
+                if tile == TileContent.Resource and self.PlayerInfo.CarriedResources < self.PlayerInfo.CarryingCapacity:
                     return create_collect_action(self.moves[i])
         except Exception as e:
             print(e)
