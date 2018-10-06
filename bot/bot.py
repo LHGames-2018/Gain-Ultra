5028c81898dc6f0b5d8736a44be63fefac2c823a
alizeegagnon <alizeegagnon602@hotmail.com>
2018-10-06 14:28:11 -0400

    decision tree

--- bot/bot.py
+++ bot/bot.py
@@ -1,4 +1,5 @@
 from helper import *
+
 from random import randint
 
 class Bot:
@@ -6,6 +7,7 @@ class Bot:
     def __init__(self):
         movement = self.hardcode_turn()
         self.moves = [Point(1,0), Point(0,1), Point(-1,0), Point(0,-1)]
+        self.mode = (1, 0, 0, 0)  # onehot: first is collect resource, second is find shoppe, third is ATTACK RECKLESSLY, fourth is go home even if pack not full
 
     def before_turn(self, playerInfo):
         """
@@ -32,9 +34,9 @@ class Bot:
             :param gameMap: The gamemap.
             :param visiblePlayers:  The list of visible players.
         """
+        print(self.mode)
         try:
-            action = self.PlayerInfo.do_decision(gameMap)
-
+            action= self.do_decision(gameMap)
         # Write your bot here. Use functions from aiHelper to instantiate your actions.
             return action
         except Exception as e:
@@ -46,3 +48,46 @@ class Bot:
         """
         pass
 
+    def do_decision(self, gamemap):
+        if self.PlayerInfo.Position == self.PlayerInfo.HouseLocation:
+            self.mode = (1,0,0,0)
+            #if self.TotalResources >= 10000: #or other upgrade goal:
+             #   return self.buy_upgrade()
+        if self.mode[3] == 1 or self.PlayerInfo.CarriedResources>=self.PlayerInfo.CarryingCapacity:
+            return self.go_home(gamemap)
+        elif self.mode[0] == 1 :
+            return self.mine_nearest_resource(gamemap)
+        else:
+            return None
+
+    def mine_nearest_resource(self, gamemap):
+        res, dist = find_nearest_resource(gamemap, self.PlayerInfo)
+        if res:
+            if dist == 1:
+                print("MINING")
+                print(str(self.PlayerInfo.CarriedResources))
+                return create_collect_action(res.Position - self.PlayerInfo.Position)
+            else:
+                #Call find empty spot here
+                emptyres, emptydist = find_empty_spot(gamemap, self.PlayerInfo, res.Position)
+                print("Trying to find empty spot to mine")
+                if emptyres:
+                    return self.move_to(gamemap, emptyres)
+                else:
+                    return self.go_home(gamemap)
+        else:
+            return self.go_home(gamemap)
+
+    def go_home(self, gamemap):
+        print("GOING HOME")
+        self.mode=(0,0,0,1)
+        return (self.move_to(gamemap, self.PlayerInfo.HouseLocation))
+
+    def move_to(self, gamemap, target):
+        path = a_star(gamemap, self.PlayerInfo, target)
+        if path:
+            next_tile = path.pop().position
+            direction = next_tile - self.PlayerInfo.Position
+            return create_move_action(direction)
+        else:
+            return self.go_home(gamemap)
--- helper/player.py
+++ helper/player.py
@@ -21,60 +21,18 @@ class Player:
         self.Score = score
         self.Name = name
         self.UpgradeLevels = upgradeLevels
-        self.mode = (1,0,0,0) #onehot: first is collect resource, second is find shoppe, third is ATTACK RECKLESSLY, fourth is go home even if pack not full
+
 
     def getUpgradeLevel(self, type):
         return self.UpgradeLevels[type]
 
-    def move_to(self, gamemap, target):
-        path = a_star(gamemap, self, target)
-        if path:
-            next_tile = path.pop().position
-            direction = next_tile - self.Position
-            return create_move_action(direction)
-        else:
-            return self.go_home(gamemap)
-
-    def go_home(self, gamemap):
-        print("GOING HOME")
-        self.mode = (0,0,0,1)
-        return self.move_to(gamemap, self.HouseLocation)
 
     def buy_upgrade(self):#TODO
         return create_upgrade_action(UpgradeType.CarryingCapacity)
 
-    def mine_nearest_resource(self, gamemap):
-        res, dist = find_nearest_resource(gamemap, self)
-        if res:
-            if dist == 1:
-                print("MINING")
-                print(str(self.CarriedResources))
-                return create_collect_action(res.Position - self.Position)
-            else:
-                #Call find empty spot here
-                emptyres, emptydist = find_empty_spot(gamemap, self, res.Position)
-                print("Trying to find empty spot to mine")
-                if emptyres:
-                    return self.move_to(gamemap, emptyres)
-                else:
-                    return self.go_home(gamemap)
-        else:
-            return self.go_home(gamemap)
 
 
-    def do_decision(self, gamemap):
-        print("doing decisioin, mode =")
-        print(self.mode)
-        if self.Position == self.HouseLocation:
-            self.mode = (1,0,0,0)
-            if self.TotalResources >= 10000: #or other upgrade goal:
-                return self.buy_upgrade()
-        if self.mode[3]==1 or self.CarriedResources>=self.CarryingCapacity:
-            return self.go_home(gamemap)
-        elif self.mode[0]==1 :
-            return self.mine_nearest_resource(gamemap)
-        else:
-            return None
+
 
 
 
