91b2ef2578eb6567266a2ed4d479a23a93f79f67
Fudgyking <escalado19@hotmail.com>
2018-10-06 11:55:18 -0400

    Upgrade logic

--- bot/bot.py
+++ bot/bot.py
@@ -4,6 +4,8 @@ from random import randint
 class Bot:
 
     def __init__(self):
+        self.upgradeOrder = [ UpgradeType.CarryingCapacity, UpgradeType.CollectingSpeed, UpgradeType.AttackPower, UpgradeType.Defence, UpgradeType.MaximumHealth]
+        self.upgradePrices = [10000, 15000,	25000, 50000, 100000]
         movement = self.hardcode_turn()
         self.moves = [Point(1,0), Point(0,1), Point(-1,0), Point(0,-1)]
 
@@ -33,6 +35,11 @@ class Bot:
             :param visiblePlayers:  The list of visible players.
         """
         try:
+            if self.PlayerInfo.TotalResources > 0 and self.PlayerInfo.Position == self.PlayerInfo.HouseLocation:
+                for i in range(len(self.upgradeOrder)):
+                    level = self.PlayerInfo.getUpgradeLevel(self.upgradeOrder[i])
+                    if self.PlayerInfo.TotalResources >= self.upgradePrices[level]:
+                        return create_upgrade_action(self.upgradeOrder[i])
             action = self.PlayerInfo.mine_nearest_resource(gameMap)
         # Write your bot here. Use functions from aiHelper to instantiate your actions.
             return action
